28d8ca4a009e95ede62f34ff4515180c3ad7ca3e
bigsolucoes <104033588+bigsolucoes@users.noreply.github.com>
2025-08-13 23:43:04 -0300

    Add pages/api/index.js 1755139384306

---
+++ pages/api/index.js
@@ -0,0 +1,9 @@
+// Dentro de pages/index.js
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Minha Página Inicial</h1>
+      <p>Bem-vindo ao meu site!</p>
+    </div>
+  );
+}
